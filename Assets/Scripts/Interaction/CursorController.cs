ab3ebf76dabaebae4106e43d04ceff8ad4a7de27
Tim Beier <tim.beier@haw-hamburg.de>
2020-01-31 22:39:53 +0100

    extracted logic from CursorController to SnappingController

--- Assets/Scripts/Interaction/CursorController.cs
+++ Assets/Scripts/Interaction/CursorController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Interaction.Dragging;
+using Interaction.Snapping;
 using UnityEngine;
 
 namespace Interaction
@@ -10,11 +12,13 @@ namespace Interaction
         private SpriteRenderer _spriteRenderer;
         private Camera _mainCamera;
         private IDraggable _draggable;
+        private ISnappingController _snappingController;
 
         private void Awake()
         {
             _mainCamera = Camera.main;
             _spriteRenderer = GetComponent<SpriteRenderer>();
+            _snappingController = new SnappingController();
         }
         
         private void OnTriggerStay2D(Collider2D other)
@@ -68,49 +72,8 @@ namespace Interaction
         {
             if (_draggable == null) return;
             
-            TrySnapDraggable(_draggable);
-        }
-        
-        //ToDo move into another script
-
-        private void TrySnapDraggable(IDraggable draggable)
-        {
-            List<ISnapable> snapables = draggable.GetSnapables();
-            foreach (ISnapable snapable in snapables)
-            {
-                ISnapable targetSnapable = snapable.GetTriggeredSnapable();
-                if (targetSnapable != null)
-                {
-                    SnapDraggable(snapable, targetSnapable);
-                    return;
-                }
-            }
+            _snappingController.TrySnapDraggable(_draggable);
         }
 
-        private void SnapDraggable(ISnapable selected, ISnapable target)
-        {
-            //ToDo check for left/right/up/down pos?
-            Transform targetTransform = target.GetTransform();
-            Transform selectedParentTransform = selected.GetTransform().parent.transform;
-            float xOffset = selected.GetTransform().position.x - selectedParentTransform.position.x;
-            float yOffset = selected.GetTransform().position.y - selectedParentTransform.position.y;
-            Vector2 targetPosition = new Vector2(targetTransform.position.x - xOffset, targetTransform.position.y -yOffset);
-
-            //Debug.Log("xOffset: " + xOffset + "/yOffset: " + yOffset);
-            //Debug.Log("curPos: " + selectedParentTransform.position);
-            //Debug.Log("targetPos: " + targetPosition);
-            
-            selectedParentTransform.position = targetPosition;
-            
-            //Move to target BlockContainer
-            GameObject selectedContainer = selectedParentTransform.parent.gameObject;
-            selectedParentTransform.parent = targetTransform.parent.parent;
-            Destroy(selectedContainer);
-            
-            //Deactivates used Snapables
-            selected.SetSnapped();
-            target.SetSnapped();
-        }
-        
     }
 }
