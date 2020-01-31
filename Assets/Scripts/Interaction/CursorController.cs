b75b04251be66b0ba4e65a5961f2c65f99d22db6
Tim Beier <tim.beier@haw-hamburg.de>
2020-01-31 21:54:50 +0100

    fixed snapping

--- Assets/Scripts/Interaction/CursorController.cs
+++ Assets/Scripts/Interaction/CursorController.cs
@@ -22,7 +22,7 @@ namespace Interaction
             IDraggable draggable = other.GetComponent<IDraggable>();
             if (draggable == null) return;
 
-            draggable.Highlight();
+           // draggable.Highlight();
             _draggables.Add(draggable);
         }
     
@@ -31,7 +31,7 @@ namespace Interaction
             IDraggable draggable = other.GetComponent<IDraggable>();
             if (draggable == null) return;
 
-            draggable.UnHighlight();
+         //   draggable.UnHighlight();
             _draggables.Remove(draggable);
         }
 
@@ -61,7 +61,47 @@ namespace Interaction
     
         private void MouseUp()
         {
+            foreach (IDraggable draggable in _draggables)
+            {
+                bool snapped = TrySnapDraggable(draggable);
+                if (snapped) return;
+            }
             _draggables.Clear();
         }
+        
+        //ToDo move into another script
+
+        private bool TrySnapDraggable(IDraggable draggable)
+        {
+            List<ISnapable> snapables = draggable.GetSnapables();
+            foreach (ISnapable snapable in snapables)
+            {
+                ISnapable targetSnapable = snapable.GetTriggeredSnapable();
+                if (targetSnapable != null)
+                {
+                    SnapDraggable(snapable, targetSnapable);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void SnapDraggable(ISnapable selected, ISnapable target)
+        {
+            //ToDo check for left/right/up/down pos?
+            Transform targetTransform = target.GetTransform();
+            Transform selectedParentTransform = selected.GetTransform().parent.transform;
+            float xOffset = selected.GetTransform().position.x - selectedParentTransform.position.x;
+            float yOffset = selected.GetTransform().position.y - selectedParentTransform.position.y;
+            Vector2 targetPosition = new Vector2(targetTransform.position.x - xOffset, targetTransform.position.y -yOffset);
+
+            //Debug.Log("xOffset: " + xOffset + "/yOffset: " + yOffset);
+            //Debug.Log("curPos: " + selectedParentTransform.position);
+            //Debug.Log("targetPos: " + targetPosition);
+            
+            selectedParentTransform.position = targetPosition;
+        }
+        
     }
 }
