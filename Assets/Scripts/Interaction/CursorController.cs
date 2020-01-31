7e3ed081e42574b9fcc18c055bbb7d7f7b50d816
Tim Beier <tim.beier@haw-hamburg.de>
2020-01-31 20:17:11 +0100

    added CursorController

---
+++ Assets/Scripts/Interaction/CursorController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Interaction
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    class CursorController : MonoBehaviour
+    {
+
+        private SpriteRenderer _spriteRenderer;
+        private Camera _mainCamera;
+        private List<IDraggable> _draggables = new List<IDraggable>();
+
+        private void Awake()
+        {
+            _mainCamera = Camera.main;
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            IDraggable draggable = other.GetComponent<IDraggable>();
+            if (draggable == null) return;
+
+            draggable.Highlight();
+            _draggables.Add(draggable);
+        }
+    
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            IDraggable draggable = other.GetComponent<IDraggable>();
+            if (draggable == null) return;
+
+            draggable.UnHighlight();
+            _draggables.Remove(draggable);
+        }
+
+        private void Update()
+        {
+            UpdateMousePosition();
+            
+            if(Input.GetMouseButton(0))
+                MouseDown();
+            if(Input.GetMouseButtonUp(0))
+                MouseUp();
+        }
+
+        private void UpdateMousePosition()
+        {
+            Vector2 pos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = pos;
+        }
+
+        private void MouseDown()
+        {
+            foreach (IDraggable draggable in _draggables)
+            {
+                draggable.UpdatePosition(transform.position);
+            }
+        }
+    
+        private void MouseUp()
+        {
+            _draggables.Clear();
+        }
+    }
+}
