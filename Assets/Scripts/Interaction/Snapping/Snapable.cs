f037d696ae90efbcc827402ca86aa297994f0bbf
Tim Beier <tim.beier@haw-hamburg.de>
2020-01-31 22:39:17 +0100

    moved snapping to sub dir

---
+++ Assets/Scripts/Interaction/Snapping/Snapable.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Interaction.Snapping
+{
+    [RequireComponent(typeof(Collider2D))]
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Snapable: MonoBehaviour, ISnapable
+    {
+       // [SerializeField] private List<Transform> snapTransforms;
+
+        private SpriteRenderer _spriteRenderer;
+        private Color _baseColor;
+        private readonly Color _highlightColor = Color.black;
+        private ISnapable _triggeredSnapable;
+
+        private bool snapped = false;
+        
+        public ISnapable GetTriggeredSnapable()
+        {
+            if (snapped) return null;
+            
+            return _triggeredSnapable;
+        }
+
+        public Transform GetTransform()
+        {
+            return transform;
+        }
+
+        public void SetSnapped()
+        {
+            snapped = true;
+            _spriteRenderer.color = _baseColor;
+        }
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _baseColor = _spriteRenderer.color;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (snapped) return;
+            
+            ISnapable snapable = other.GetComponent<ISnapable>();
+            if (snapable == null) return;
+
+            _spriteRenderer.color = _highlightColor;
+            _triggeredSnapable = snapable;
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (snapped) return;
+            
+            ISnapable snapable = other.GetComponent<ISnapable>();
+            if (snapable == null) return;
+
+            _spriteRenderer.color = _baseColor;
+            _triggeredSnapable = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Snapping/SnappingController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Interaction.Containers;
+using Interaction.Dragging;
+using UnityEngine;
+
+namespace Interaction.Snapping
+{
+    class SnappingController: ISnappingController
+    {
+        public void TrySnapDraggable(IDraggable draggable)
+        {
+            List<ISnapable> snapables = draggable.GetSnapables();
+            foreach (ISnapable snapable in snapables)
+            {
+                ISnapable targetSnapable = snapable.GetTriggeredSnapable();
+                if (targetSnapable != null)
+                {
+                    SnapDraggable(snapable, targetSnapable);
+                    return;
+                }
+            }
+        }
+
+        private void SnapDraggable(ISnapable selected, ISnapable target)
+        {
+            //ToDo check for left/right/up/down pos?
+            Transform targetTransform = target.GetTransform();
+            Transform selectedParentTransform = selected.GetTransform().parent.transform;
+            float xOffset = selected.GetTransform().position.x - selectedParentTransform.position.x;
+            float yOffset = selected.GetTransform().position.y - selectedParentTransform.position.y;
+            Vector2 targetPosition = new Vector2(targetTransform.position.x - xOffset, targetTransform.position.y -yOffset);
+
+            //Debug.Log("xOffset: " + xOffset + "/yOffset: " + yOffset);
+            //Debug.Log("curPos: " + selectedParentTransform.position);
+            //Debug.Log("targetPos: " + targetPosition);
+            
+            selectedParentTransform.position = targetPosition;
+            
+            //Move to target BlockContainer
+            IShapeContainer toDestroy = selectedParentTransform.parent.GetComponent<IShapeContainer>();
+            selectedParentTransform.parent = targetTransform.parent.parent;
+            toDestroy.Destroy();
+            
+            //Deactivates used Snapables
+            selected.SetSnapped();
+            target.SetSnapped();
+        }
+        
+    }
+}
