26b04f6073e1e59ee6477624cbc733531426bdd0
Tim Beier <tim.beier@haw-hamburg.de>
2020-01-31 20:17:21 +0100

    added Draggables

---
+++ Assets/Scripts/Interaction/Draggable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Interaction
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Draggable: MonoBehaviour, IDraggable
+    {
+        private SpriteRenderer _spriteRenderer;
+        private Color _baseColor;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _baseColor = _spriteRenderer.color;
+        }
+
+        public void Highlight()
+        {
+            _spriteRenderer.color = Color.black;
+        }
+
+        public void UnHighlight()
+        {
+            _spriteRenderer.color = _baseColor;
+        }
+
+        public void UpdatePosition(Vector2 pos)
+        {
+            transform.position = pos;
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/IDraggable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Interaction
+{
+    public interface IDraggable
+    {
+        void Highlight();
+        void UnHighlight();
+        void UpdatePosition(Vector2 pos);
+    }
+}
