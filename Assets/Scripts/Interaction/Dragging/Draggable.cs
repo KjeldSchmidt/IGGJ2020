5fd1ab68c1fc8ef10780fb48c7ed6299e6bff7a8
Tim Beier <tim.beier@haw-hamburg.de>
2020-01-31 22:39:28 +0100

    moved draggables to sub dir

---
+++ Assets/Scripts/Interaction/Dragging/Draggable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Interaction.Snapping;
+using UnityEngine;
+
+namespace Interaction.Dragging
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Draggable: MonoBehaviour, IDraggable
+    {
+        [SerializeField] private List<Snapable> snapables;
+        private SpriteRenderer _spriteRenderer;
+        private Color _baseColor;
+        
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _baseColor = _spriteRenderer.color;
+        }
+
+        public void Highlight()
+        {
+            _spriteRenderer.color = Color.black;
+        }
+
+        public void UnHighlight()
+        {
+            _spriteRenderer.color = _baseColor;
+        }
+
+        public void UpdatePosition(Vector2 pos)
+        {
+            transform.position = pos;
+        }
+        
+        public Transform GetBlockContainerTransform()
+        {
+            return transform.parent;
+        }
+        
+        public List<ISnapable> GetSnapables()
+        {
+            //Todo make pretty
+            return new List<ISnapable>(snapables);
+        }
+    }
+}
