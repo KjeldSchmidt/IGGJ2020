bee9516a69f931ccd7a3bf1c4142d115798f704b
Tim Beier <tim.beier@haw-hamburg.de>
2020-02-01 18:58:46 +0100

    finished startbutton animation

--- Assets/Scripts/Interaction/StartGameButton.cs
+++ Assets/Scripts/Interaction/StartGameButton.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Interaction
 {
     public class StartGameButton: MonoBehaviour
     {
         [SerializeField] private List<SpriteRenderer> spriteRenderers;
-    
+        [SerializeField] private List<TextMesh> textMeshes;
+        [SerializeField] private Animation startAnimation;
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
+
             if (other.GetComponent<CursorController>() == null) return;
             
             foreach (SpriteRenderer spriteRenderer in spriteRenderers)
             {
-                spriteRenderer.color = Color.white;
+                Color targetColor = Color.black;
+                if (spriteRenderer.color == targetColor) targetColor = Color.white;
+                spriteRenderer.color = targetColor;
             }
+            foreach (TextMesh textMesh in textMeshes)
+            {
+                Color targetColor = Color.black;
+                if (textMesh.color == targetColor) targetColor = Color.white;
+                textMesh.color = targetColor;
+            }
+        }
+
+        private void OnMouseUp()
+        {
+            
+            startAnimation.Play();
         }
 
         private void OnTriggerExit2D(Collider2D other)
@@ -23,7 +42,15 @@ namespace Interaction
             
             foreach (SpriteRenderer spriteRenderer in spriteRenderers)
             {
-                spriteRenderer.color = Color.black;
+                Color targetColor = Color.black;
+                if (spriteRenderer.color == targetColor) targetColor = Color.white;
+                spriteRenderer.color = targetColor;
+            }
+            foreach (TextMesh textMesh in textMeshes)
+            {
+                Color targetColor = Color.black;
+                if (textMesh.color == targetColor) targetColor = Color.white;
+                textMesh.color = targetColor;
             }
         }
     }
