0fabe7d1a2171ecf537e848c32e69fc9e7931f90
Tim Beier <tim.beier@haw-hamburg.de>
2020-02-01 14:49:55 +0100

    added basic explosion script

---
+++ Assets/Explosion.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(1))
+        {
+            transform.localScale = new Vector3(Random.Range(0, 10),Random.Range(0, 10),1);
+        }
+    }
+}
