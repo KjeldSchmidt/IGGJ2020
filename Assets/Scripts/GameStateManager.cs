c3f8e016e013f3a135a757f4747546e66fb9b961
Tim Beier <tim.beier@haw-hamburg.de>
2020-02-02 03:56:11 +0100

    testing

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,46 @@
+﻿using Spawning;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] private Counter counter;
+    [SerializeField] private Animation bulldozerAnimation;
+    [SerializeField] private Level1Controller level1Controller;
+    
+    private int _stateId = 0;
+    void Update()
+    {
+        switch (_stateId)
+        {
+            case 0:
+                ShovelCorpses();
+                break;
+            case 1:
+                level1Controller.StartLevel(this);
+                break;
+            case 2:
+                //ToDo Damage GrimReaper & move him behind wall
+                //ToDo Throw in Tractor parts to demolish wall
+                break;
+            case 3:
+                //ToDo Damage GrimReaper & make him leave
+                //ToDo Show Credits
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ShovelCorpses()
+    {
+        if (counter.GetCount() < 100) return;
+        bulldozerAnimation.Play();
+        _stateId++;
+    }
+
+    public void FinishLevel1()
+    {
+        _stateId++;
+    }
+}
---
+++ Assets/Scripts/Level1Controller.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Level1Controller : MonoBehaviour
+{
+    [SerializeField] private Animation moveReaperToTop;
+    [SerializeField] private GameObject level1ShapesContainer;
+    [SerializeField] private GameObject level1AbilitiesContainer;
+    
+    private GameStateManager _gameStateManager;
+    
+    // Start is called before the first frame update
+    public void StartLevel(GameStateManager gameStateManager)
+    {
+        _gameStateManager = gameStateManager;
+        
+        //Move Reaper to Top
+        moveReaperToTop.Play();
+        DestroyRagDolls();
+        DropLevelShapes();
+    }
+
+    private void DestroyRagDolls()
+    {
+        //todo destroy ragdolls
+    }
+
+    private void DropLevelShapes()
+    {
+        level1ShapesContainer.SetActive(true);
+        level1AbilitiesContainer.SetActive(true);
+    }
+    
+    private void FinishLevel()
+    {
+        _gameStateManager.FinishLevel1();
+    }
+}
